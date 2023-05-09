51b3de401a697b7e54b262eb949c3f9e4aa74a62
boxyboxy <eugenengboxiang@gmail.com>
2023-05-09 21:59:52 +0800

    created django project and app

---
+++ django-commands.sh
@@ -0,0 +1,23 @@
+vagrant ssh
+cd /vagrant
+## creating a python environment in the vm, at the home directory of the vagrant server
+python -m venv ~/env
+## activating a virtual environment
+source ~/env/bin/activate
+## to deactivate
+deactivate
+
+## install required python packages, django and django rest framework
+pip install -r requirements.txt
+
+##initialising and creating a django project
+django-admin.py startproject profiles_project .
+
+## creating an app within project 
+python manage.py startapp profiles_api
+
+## enable profiles api app in our django app
+edit settings.py under profiles_project, add in the dependencies in the installed app
+
+## test changes to a django project by using the django development server
+python manage.py runserve 0.0.0.0:8000
